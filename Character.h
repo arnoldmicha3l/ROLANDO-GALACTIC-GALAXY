c6b87cfc41f6b3d194e4718073bdaae1ad018a7c
Kyle <kyleski@users.noreply.github.com>
2025-11-30 18:33:47 +0800

    Finalizing C++ implementations and fixing linker errors

--- Character.h
+++ Character.h
@@ -3,53 +3,56 @@
 
 #include <string>
 #include <vector>
-using namespace std;
-
-struct Skill {
-    string name;
-    string description;
-    int manaCost;
-    bool isOneHitDelete;
-};
+#include "Skill.h"
 
 class Character {
+private:
+    std::string name;
+    std::string title;
+    int maxHP;
+    int hp;
+    int maxMana;
+    int mana;
+    int baseDamage;
+    std::string passiveDesc;
+    std::string bio;
+    std::string grudge;
+    std::vector<Skill> skills;
+
 public:
     Character();
-    Character(const string &name,
-              const string &title,
+    Character(const std::string &name,
+              const std::string &title,
               int maxHP,
               int maxMana,
               int baseDamage,
-              const string &passiveDesc);
+              const std::string &passiveDesc,
+              const std::string &bio,
+              const std::string &grudge);
 
     void resetForNewRound();
 
-    const string &getName() const;
-    const string &getTitle() const;
+    const std::string &getName() const;
+    const std::string &getTitle() const;
     int getHP() const;
     int getMana() const;
     int getMaxHP() const;
     int getMaxMana() const;
     int getBaseDamage() const;
-    const string &getPassiveDesc() const;
-    const vector<Skill> &getSkills() const;
+    const std::string &getPassiveDesc() const;
+    const std::vector<Skill> &getSkills() const;
+
+    const std::string &getBio() const;
+    const std::string &getGrudge() const;
+
+    void setHP(int value);
+    void setMaxHP(int value);
 
     void addSkill(const Skill &skill);
     void takeDamage(int amount);
     void heal(int amount);
     void useMana(int amount);
     bool isAlive() const;
-
-private:
-    string name;
-    string title;
-    int maxHP;
-    int hp;
-    int maxMana;
-    int mana;
-    int baseDamage;
-    string passiveDesc;
-    vector<Skill> skills;
 };
 
 #endif
--- Game.h
+++ Game.h
@@ -1,35 +1,36 @@
 #ifndef GAME_H
 #define GAME_H
-
+#include "Character.h"
 #include <vector>
 #include <random>
-#include "Character.h"
 using namespace std;
+
 
 enum class GameMode { PVP, PVC };
 
 class Game {
 public:
-    Game();
-    void run();
+Game();
+void run();
+void clearScreen();
+bool confirmExit();
 
 private:
-    vector<Character> roster;
-    mt19937 rng;
+vector<Character> roster;
+std::mt19937 rng;
 
-    void initRoster();
-    void showIntro();
-    void mainMenu();
-    void playPVP();
-    void playPVC();
+void initRoster();
+void showIntro();
+void mainMenu();
+void playPVP();
+void playPVC();
+Character chooseCharacter(int playerNumber, bool showGrudges, int forbiddenIndex);
+int chooseSkill(const Character &ch);
+int getRandomInt(int min, int max);
+int computeDamage(Character &attacker, Character &defender, const Skill &skill);
+bool handleLowHP(Character &ch, int playerNumber, GameMode mode, bool isHuman);
+void printCharacterCard(const Character &ch, int index);
 
-    Character chooseCharacter(int playerNumber);
-    int chooseSkill(const Character &ch);
-    int getRandomInt(int min, int max);
-
-    int computeDamage(Character &attacker, Character &defender, const Skill &skill);
-    bool handleLowHP(Character &ch, int playerNumber, GameMode mode, bool isHuman);
-    void printCharacterCard(const Character &ch, int index);
 };
 
 #endif // GAME_H
