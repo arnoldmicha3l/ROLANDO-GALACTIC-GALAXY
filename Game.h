6d8ab1289c5c4b8da420a3ecbfc7f83c58259132
arnoldmicha3l <arnoldmichaeltabada@gmail.com>
2025-12-02 11:28:59 +0800

    Fix Game.h merge conflict and update settings

--- Game.h
+++ Game.h
@@ -1,3 +1,4 @@
+// Game.h
 #ifndef GAME_H
 #define GAME_H
 
@@ -47,41 +48,6 @@ private:
 
     void playPVP();
     void playPVC();
-};
-
-#endif // GAME_H
-#ifndef GAME_H
-#define GAME_H
-
-#include <vector>
-#include <random>
-#include "Character.h"
-using namespace std;
-
-enum class GameMode { PVP, PVC };
-
-class Game {
-public:
-    Game();
-    void run();
-
-private:
-    vector<Character> roster;
-    mt19937 rng;
-
-    void initRoster();
-    void showIntro();
-    void mainMenu();
-    void playPVP();
-    void playPVC();
-
-    Character chooseCharacter(int playerNumber);
-    int chooseSkill(const Character &ch);
-    int getRandomInt(int min, int max);
-
-    int computeDamage(Character &attacker, Character &defender, const Skill &skill);
-    bool handleLowHP(Character &ch, int playerNumber, GameMode mode, bool isHuman);
-    void printCharacterCard(const Character &ch, int index);
 };
 
 #endif // GAME_H
